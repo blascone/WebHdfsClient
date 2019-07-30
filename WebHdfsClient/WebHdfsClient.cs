ceb9b3de679524cf6c0af9b64f5893073b05ecd5
blascone <blascone@blascone.it>
2019-07-30 15:43:33 +0200

    first commit

---
+++ WebHdfsClient/WebHdfsClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+
+namespace WebHdfsClient {
+
+    public class WebHdfsClient {
+
+        private HttpWebRequest client;
+        private readonly string hdfsUrl;
+
+        public WebHdfsClient(string host, string port = "50075", string version = "v1") {
+            hdfsUrl = $"http://{host}:{port}/webhdfs/{version}";
+        }
+
+
+        private string GetRequest(string folder, string requestType, Dictionary<string, string> optionalParameters = null) {
+            var optionalparams = "";
+            if (optionalParameters != null) {
+                optionalparams = string.Join("&", optionalParameters.Select(x => x.Key + "=" + x.Value).ToArray());
+            }
+
+            var urlrequest = $"{hdfsUrl}/{folder}/?op={requestType}&{optionalparams}";
+            Console.WriteLine(urlrequest);
+            client = (HttpWebRequest) WebRequest.Create(urlrequest);
+            var response = client.GetResponse();
+            var stream = response.GetResponseStream();
+            var sr = new StreamReader(stream);
+            var content = sr.ReadToEnd();
+            return content;
+        }
+
+
+        public string Open(string folder, int offset = 0, string contentlength = null, string buffersize = null) {
+            var parameters = new Dictionary<string, string>();
+            if (offset != 0) parameters.Add("offset", offset.ToString());
+            if (contentlength != null) parameters.Add("length", contentlength);
+            if (buffersize != null) parameters.Add("buffersize", buffersize);
+
+            return GetRequest(folder, "OPEN", parameters);
+        }
+
+
+        public string FileStatus(string folder) {
+            return GetRequest(folder, "GETFILESTATUS");
+        }
+
+        public string ListStatus(string folder) {
+            return GetRequest(folder, "LISTSTATUS");
+        }
+
+    }
+
+}
---
+++ WebHdfsClientTest/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace WebHdfsClientTest {
+
+    [TestFixture]
+    public class Tests {
+
+        [Test]
+        public void FileStatusTest() {
+            WebHdfsClient.WebHdfsClient client = new WebHdfsClient.WebHdfsClient("10.150.0.10","50070");
+           Console.WriteLine(client.FileStatus("ciop/run/hands-on-1/0000001-190705175000140-oozie-oozi-W"));
+        }
+        
+        
+        [Test]
+        public void ListStatusTest() {
+            WebHdfsClient.WebHdfsClient client = new WebHdfsClient.WebHdfsClient("10.150.0.10","50070");
+            Console.WriteLine(client.ListStatus("ciop/run/hands-on-1/0000001-190705175000140-oozie-oozi-W"));
+        }
+        
+     
+        [Test]
+        public void OpenTest() {
+            WebHdfsClient.WebHdfsClient client = new WebHdfsClient.WebHdfsClient("10.150.0.10","50070");
+
+            Console.WriteLine(client.Open("ciop/run/tg-drm-integration/0000000-190416163227986-oozie-oozi-W/workflow-params.xml"));
+        }
+
+    }
+
+}
